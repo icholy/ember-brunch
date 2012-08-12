130ffa7e0bf2eed5d11f6d0e56daf015c7432742
ilia choly <ilia.choly@gmail.com>
2012-08-12 19:28:29 -0400

    remover namespaces

--- app/app.js
+++ app/app.js
@@ -1,9 +1,3 @@
 // Application bootstrapper
 
-var App = Em.Application.create({
-    Views       : Em.Namespace.create(),
-    Models      : Em.Namespace.create(),
-    Controllers : Em.Namespace.create(),
-});
-
-module.exports = App;
+module.exports = Em.Application.create();
