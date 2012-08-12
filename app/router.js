d1ee79809fa172379d7baf5c0dbfdef088d374fc
ilia choly <ilia.choly@gmail.com>
2012-08-12 19:29:43 -0400

    connectOutlets

--- app/router.js
+++ app/router.js
@@ -3,14 +3,18 @@ var App = require('app');
 App.Router = Em.Router.extend({
     enableLogging: true,
     root: Em.Route.extend({
+        
         index: Em.Route.extend({
             route: '/',
             redirectsTo: 'home'
         }),
+
         home: Em.Route.extend({
+            
             route: '/home',
-            connectOutlets: function(route){
-                console.log('home');
+
+            connectOutlets: function(router, context){
+                router.get('applicationController').connectOutlet('home');
             }
         })
     })
