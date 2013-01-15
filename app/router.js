08ba7d80fb97f3b70b9ec895f8057976f9b2fd7f
seankeating <sean@igniite.com>
2013-01-14 19:51:27 -0800

    Replace quotation marks with apostrophes

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 var App = require('app');
 
 App.Router.map(function() {
-	this.route("index", { path: "/"});
-	this.route("home");
-	this.route("bob");
+	this.route('index', { path: '/'});
+	this.route('home');
+	this.route('bob');
 });
