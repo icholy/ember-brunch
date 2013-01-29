67335409a84cec6a7861e3de8d875edc69e80af5
Ilia Choly <icholy@accipiterradar.com>
2013-01-29 12:17:59 -0500

    remove files I thought were already removed ...

--- app/controllers/application.js
+++
@@ -1,5 +0,0 @@
-var App = require('app');
-
-App.ApplicationController = Em.Controller.extend({
-
-});
--- app/views/application.js
+++
@@ -1,5 +0,0 @@
-var App = require('app');
-
-App.ApplicationView = Em.View.extend({
-    templateName: 'templates/application'
-});
